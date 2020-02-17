e2116365a519e4578f92f661bb710e78fb99f9fb
Patryk Kostrzewski <pjkostrzewski@gmail.com>
2020-02-17 23:11:24 +0100

    get offer id added

--- database.py
+++ database.py
@@ -1,23 +1,22 @@
 import psycopg2
+import getpass
 
-
+password = getpass.getpass("password: ", stream=None)
 try:
     connection = psycopg2.connect(  user = "postgres",
-                                    password = "password",
+                                    password = password,
                                     host = "127.0.0.1",
                                     port = "5432",
                                     database = "postgres")
     cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")                               
+    print(  connection.get_dsn_parameters(),"\n")                               
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
     print("You are connected to - ", record,"\n")
 except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL", error)
 finally:
-    #closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from proxies import Proxy
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+import re
 
 
 def get_today_date():
@@ -15,6 +16,9 @@ def get_date_with_timedelta(days):
     date = datetime.now() + timedelta(days=days)
     return date.strftime("%Y-%m-%d")
 
+def get_offer_id_from_url(url):
+    return re.findall(pattern=r".*ofr_id=(.{64})&.*", string=url)[0]
+    
 '''
 https://www.itaka.pl/last-minute/
 ?departureDate=2020-02-13
@@ -37,43 +41,24 @@ print(url)
 # response = requests.get(url=url, proxies={"http": f"http://{proxy}","https": f"http://{proxy}"}).text
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
 
-# header variable
 headers = { 'User-Agent' : user_agent }
 response = requests.get(url=url, headers=headers).text
 soup = BeautifulSoup(response, "html.parser")
 article = soup.find_all('article', {'class': "offer clearfix"})
-# for offer in article:
-#     try:
-#         tag = offer.find('span', class_='old-price_value').get_text()
-#     except:
-#         tag = None
-#     current_price_value = offer.find('span', class_='current-price_value').get_text()
-#     hotel_rank = offer.find('span', class_='hotel-rank').get_text()
-#     link = offer.find('a', {'class': 'offer_link pull-right'}).get('href')
-    # print(tag, current_price_value, hotel_rank, link)
-# tag = soup.find('span', class_='old-price_value')
-# current_price_value = soup.find('span', class_='current-price_value').get_text()
-# hotel_rank = soup.find('span', class_='hotel-rank').get_text()
-# link = soup.find('a', {'class': 'offer_link pull-right'}).get('href')
-def find_iter():
-    tag = soup.find('span', class_='old-price_value')
-    while tag is not None:
-        yield tag.get_text()
-        tag = tag.find_next('span', class_='old-price_value').find_next('span', class_='old-price_value')
+for offer in article:
+    try:
+        tag = offer.find('span', class_='old-price_value').get_text()
+    except:
+        tag = None
+    current_price_value = offer.find('span', class_='current-price_value').get_text()
+    hotel_rank = offer.find('span', class_='hotel-rank').get_text()
+    link = offer.find('a', {'class': 'offer_link pull-right'}).get('href')
+    print(tag, current_price_value, hotel_rank)
+    print(link)
+    print(get_offer_id_from_url(link))
+    print()
 
-gen2 = (x.get_text() for x in soup.find_all('span', class_='old-price_value')[::2])  # more pythonic way to generate all current offers
-gen = find_iter()
+# gen2 = (x.get_text() for x in soup.find_all('span', class_='old-price_value')[::2])  # more pythonic way to generate all current offers
+
 # <img src="https://i.content4travel.com/cms/img/u/desktop/seres/rktvile_0.jpg" class="figure_main-photo" alt="Hotel The Village by Rotana">
 # to get picture ^^
-
-running = True
-while running:
-    try:
-        print(next(gen2))
-    except StopIteration:
-        running = False
-# current_price_value = soup.find('span', class_='current-price_value').get_text()
-# hotel_rank = soup.find('span', class_='hotel-rank').get_text()
-# link = soup.find('a', {'class': 'offer_link pull-right'}).get('href')
-# # print(response.status_code)
-# print(old_price_value, current_price_value, hotel_rank, "https://itaka.pl{link}".format(link=link))
