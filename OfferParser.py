ba1d15f02a207eb11a8be9bbfc71e9310f2f51c6
Patryk Kostrzewski <pjkostrzewski@gmail.com>
2020-02-24 22:51:58 +0100

    add offer classes

--- OfferParser.py
+++ OfferParser.py
@@ -7,16 +7,20 @@ class OfferParser(object):
     def __init__(self, offer: bs4.element.Tag):
         self.offer = offer
 
-    def get_tag(self):
+    def _get_parsed_price(self, price: str):
+        return int(re.findall(r'(\d \d\d\d) PLN\/os', price)[0].replace(" ", ""))
+    
+    def get_old_price(self):
         try:
-            old_price = self.offer.find('span', class_='old-price_value').get_text()
+            price = self.offer.find('span', class_='old-price_value').get_text()
         except:
             return None
         finally:
-            return int(re.findall(r'(\d \d\d\d) PLN\/os', old_price)[0].replace(" ", ""))
+            return self._get_parsed_price(price)
     
-    def get_actual_price(self):
-        return self.offer.find('span', class_='current-price_value').get_text()
+    def get_current_price(self):
+        price = self.offer.find('span', class_='current-price_value').get_text()
+        return self._get_parsed_price(price)
     
     def get_hotel_rank(self):
         return self.offer.find('span', class_='hotel-rank').get_text()
@@ -27,3 +31,13 @@ class OfferParser(object):
     def get_offer_id(self):
         url = self.get_url()
         return re.findall(pattern=r".*ofr_id=(.{64})&.*", string=url)[0]
+    
+    def get_as_dict(self):
+        result = {
+            "old_price": self.get_old_price(),
+            "current_price": self.get_current_price(),
+            "ank": self.get_hotel_rank(),
+            "link": self.get_url(),
+            "offer_id": self.get_offer_id()
+        }
+        return result
