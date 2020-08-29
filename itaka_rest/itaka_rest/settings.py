142f87c70b7b55b1ba58c4bb51c2199ce29beb96
Patryk Kostrzewski <pjkostrzewski@gmail.com>
2020-08-29 23:32:41 +0200

    update settings

--- itaka_rest/itaka_rest/settings.py
+++ itaka_rest/itaka_rest/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'django_extensions',
+    'django_filters',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
