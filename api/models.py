ce8cff15deb0dab5dd82c31d84bd0b42f0fa9fb4
Patryk Kostrzewski <pjkostrzewski@gmail.com>
2020-09-22 21:48:09 +0200

    allow cur price to be null

--- api/models.py
+++ api/models.py
@@ -15,7 +15,7 @@ class Destination(models.Model):
 class Offer(models.Model):
     offer_id = models.CharField(primary_key=True, unique=True, max_length=50)
     destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='destination')
-    old_price = models.IntegerField(validators=[MinValueValidator(0)])
+    old_price = models.IntegerField(validators=[MinValueValidator(0)], blank=True, null=True)
     current_price = models.IntegerField(validators=[MaxValueValidator(old_price)])
     rank = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(6.0)])
     link = models.URLField(max_length=200)
@@ -24,3 +24,12 @@ class Offer(models.Model):
 
     def __str__(self):
         return '{} {}'.format(self.offer_id, self.destination)
+
+    @property
+    def difference(self):
+        return self.old_price - self.current_price
+
+    @property
+    def difference_percantage(self):
+        division = float(self.difference)/float(self.old_price) * 100
+        return int(round(division, 0))
