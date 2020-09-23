88d1763e462bd94e32ec61fe6d1d3f4830769115
Patryk Kostrzewski <pjkostrzewski@gmail.com>
2020-09-23 22:59:21 +0200

    use proxy as decorator

--- api/scraper/scraper.py
+++ api/scraper/scraper.py
@@ -2,47 +2,45 @@ import requests
 from requests.exceptions import ProxyError
 from bs4 import BeautifulSoup
 import re
+from functools import wraps
 from api.scraper.proxies import Proxy
 from api.scraper.Offer import Offer
 from api.scraper.OfferParser import OfferParser
 from api.scraper.helpers import (url, user_agent,
-                     get_today_date, get_date_with_timedelta,
-                     get_offer_id_from_url)
-from time import sleep
+                     get_today_date, get_date_with_timedelta)
 from api.scraper.images import get_photos_urls
 
 
 chromedriver_path = "/Users/patrykkostrzewski/Downloads/chromedriver"
 
+# TODO  add some options file
+# TODO  add some logging using Logger
+
+
+def use_proxy(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        proxy = Proxy().get()
+        proxies = {"http": f"http://{proxy}", "https": f"http://{proxy}"}
+        return func(*args, proxies=proxies, **kwargs)
+    return wrapper
+
+
+# @use_proxy
+def get_data_from_url(*args, **kwargs):
+    return requests.get(*args, **kwargs).text
+
+
+_url = url.format(departure_date=get_date_with_timedelta(days=1), date_from=get_today_date())
+headers = {'User-Agent': user_agent}
+
 
 def scrap():
-    use_proxy = False
-    _url = url.format(departure_date=get_date_with_timedelta(days=1),
-                     date_from=get_today_date())
-    headers = {'User-Agent': user_agent}
-
-    if use_proxy:
-        proxy = Proxy().get()
-        print("connected", proxy)
-        try:
-            response = requests.get(url=_url,
-                                    headers=headers,
-                                    proxies={"http": f"http://{proxy}","https": f"http://{proxy}"}
-                                    ).text
-        except ProxyError as e:
-            print(e)
-            response = requests.get(url=_url,
-                                    headers=headers).text
-    else:
-        response = requests.get(url=_url,
-                                headers=headers
-                                ).text
-
+    response = get_data_from_url(url=_url, headers=headers)
     soup = BeautifulSoup(response, "html.parser")
     article = soup.find_all('article', {'class': "offer clearfix"})
     assert len(article) > 0, "no offers found."
-
-    offers = list()
+    offers = []
     photos = get_photos_urls(chromedriver_path=chromedriver_path, url=_url)
     assert len(article) == len(photos), "numbers of offers and pictures are not equal."
     for offer, photo in zip(article, photos):
@@ -56,3 +54,4 @@ def scrap():
 
 
 print(scrap())
+
